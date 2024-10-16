d4b1332fd3cc9a5b7dd1493b1e71fcaa69fffc40
baileyf59 <baileyf59@gmail.com>
2024-10-15 23:03:58 -0400

    Dataebase created. Still working through the homepage

--- statalize_app/models.py
+++ statalize_app/models.py
@@ -1,3 +1,32 @@
+from tkinter.constants import CASCADE
+
 from django.db import models
 
-# Create your models here.
+fr= 'freshman'
+so= 'sophmore'
+jr= 'junior'
+sn= 'senior'
+
+class team (models.Model):
+    team_name = models.CharField(max_length=20)
+    wins = models.PositiveIntegerField()
+    losses = models.PositiveIntegerField()
+
+    def _str_(self):
+        return (self.team_name + " " + self.wins + " " + self.losses)
+
+class player (models.Model):
+    player = models.ForeignKey(team, on_delete=models.CASCADE)
+    player_name = models.CharField(max_length=50)
+    player_age = models.PositiveIntegerField()
+    player_year = models.Choices(fr, so, jr, sn)
+
+    def _str_(self):
+        return (self.player_name + " " + self.player_age + " " + self.player_year)
+class coach (models.Model):
+    coach = models.ForeignKey(team, on_delete=models.CASCADE)
+    coach_name = models.CharField(max_length = 50)
+    coach_year = models.PositiveIntegerField()
+
+    def _str_(self):
+        return (self.coach_name + " " + self.coach_year)
--- statalize_app/views.py
+++ statalize_app/views.py
@@ -3,9 +3,12 @@ from django.db.models import Count
 from statalize_app.models import *
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
-
+from django.http import HttpResponse
+from .models import team, player, coach
 
 # Create your views here.
 
 def display_home(request):
-    return render(request, '/home.html')
+    data = team.objects.all()
+    context = {"Teams": data}
+    return render(request, '/home.html', context)
