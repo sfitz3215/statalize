885b50ba9207cf6118aa531993018d199f74145f
baileyf59 <baileyf59@gmail.com>
2024-11-13 18:33:37 -0500

    Game dit and player edit test

--- statalize_app/forms.py
+++ statalize_app/forms.py
@@ -74,44 +74,87 @@ class AwayTeamPlayerForm(forms.Form):
         self.fields['SB'].initial = set_player.SB
         self.fields['SAC'].initial = set_player.SAC
 
-
-
-
-    def __init__(self, *args, **kwargs):
-        game = kwargs.pop('game', None)
-        super().__init__(*args, **kwargs)
-        if game:
-            self.fields['player'].queryset = player.objects.filter(plays_for=game.away_team)
-
 class HomeTeamPlayerForm(forms.ModelForm):
-    class Meta:
-        model = GamePlayerStats
-        fields = ['player', 'AB', 'BB', 'SO', 'Singles', 'Doubles', 'Triples', 'HR', 'Runs', 'RBI', 'SB', 'SAC']
-
-    def __init__(self, *args, **kwargs):
-        game = kwargs.pop('game', None)
+    AB = forms.IntegerField(min_value=0, max_value=0)
+    BB = forms.IntegerField(min_value=0, max_value=0)
+    SO = forms.IntegerField(min_value=0, max_value=0)
+    Singles = forms.IntegerField(min_value=0, max_value=0)
+    Doubles = forms.IntegerField(min_value=0, max_value=0)
+    Triples = forms.IntegerField(min_value=0, max_value=0)
+    HR = forms.IntegerField(min_value=0, max_value=0)
+    Runs = forms.IntegerField(min_value=0, max_value=0)
+    RBI = forms.IntegerField(min_value=0, max_value=0)
+    SB = forms.IntegerField(min_value=0, max_value=0)
+    SAC = forms.IntegerField(min_value=0, max_value=0)
+    def __init__(self, game, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        if game:
-            self.fields['player'].queryset = player.objects.filter(plays_for=game.home_team)
+
+        set_player = player.objects.get(team=game.away_team)
+
+        self.fields['AB'].initial = set_player.AB
+        self.fields['BB'].initial = set_player.BB
+        self.fields['SO'].initial = set_player.SO
+        self.fields['Singles'].initial = set_player.Singles
+        self.fields['Doubles'].initial = set_player.Doubles
+        self.fields['Triples'].initial = set_player.Triples
+        self.fields['HR'].initial = set_player.HR
+        self.fields['Runs'].initial = set_player.Runs
+        self.fields['RBI'].initial = set_player.RBI
+        self.fields['SB'].initial = set_player.SB
+        self.fields['SAC'].initial = set_player.SAC
 
 class AwayTeamPitcherForm(forms.ModelForm):
-    class Meta:
-        model = GamePitcherStats
-        fields = ['pitcher', 'SO', 'hits', 'walks', 'HR', 'IP', 'runs', 'ER', 'games', 'GS', 'AB']
+        SO = forms.IntegerField(min_value=0, max_value=0)
+        hits = forms.IntegerField(min_value=0, max_value=0)
+        walks = forms.IntegerField(min_value=0, max_value=0)
+        HR = forms.IntegerField(min_value=0, max_value=0)
+        IP = forms.FloatField(min_value=0, max_value=0)
+        runs = forms.IntegerField(min_value=0, max_value=0)
+        ER = forms.IntegerField(min_value=0, max_value=0)
+        games = forms.IntegerField(min_value=0, max_value=0)
+        GS = forms.IntegerField(min_value=0, max_value=0)
+        AB = forms.IntegerField(min_value=0, max_value=0)
 
-    def __init__(self, *args, **kwargs):
-        game = kwargs.pop('game', None)
-        super().__init__(*args, **kwargs)
-        if game:
-            self.fields['pitcher'].queryset = pitcher.objects.filter(plays_for=game.away_team)
+        def __init__(self, game, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+
+            set_player = player.objects.get(team=game.away_team)
+
+            self.fields['SO'].initial = set_player.AB
+            self.fields['hits'].initial = set_player.BB
+            self.fields['walks'].initial = set_player.SO
+            self.fields['HR'].initial = set_player.Singles
+            self.fields['IP'].initial = set_player.Doubles
+            self.fields['runs'].initial = set_player.Triples
+            self.fields['ER'].initial = set_player.HR
+            self.fields['games'].initial = set_player.Runs
+            self.fields['GS'].initial = set_player.RBI
+            self.fields['AB'].initial = set_player.SB
 
 class HomeTeamPitcherForm(forms.ModelForm):
-    class Meta:
-        model = GamePitcherStats
-        fields = ['pitcher', 'SO', 'hits', 'walks', 'HR', 'IP', 'runs', 'ER', 'games', 'GS', 'AB']
+    SO = forms.IntegerField(min_value=0, max_value=0)
+    hits = forms.IntegerField(min_value=0, max_value=0)
+    walks = forms.IntegerField(min_value=0, max_value=0)
+    HR = forms.IntegerField(min_value=0, max_value=0)
+    IP = forms.FloatField(min_value=0, max_value=0)
+    runs = forms.IntegerField(min_value=0, max_value=0)
+    ER = forms.IntegerField(min_value=0, max_value=0)
+    games = forms.IntegerField(min_value=0, max_value=0)
+    GS = forms.IntegerField(min_value=0, max_value=0)
+    AB = forms.IntegerField(min_value=0, max_value=0)
 
-    def __init__(self, *args, **kwargs):
-        game = kwargs.pop('game', None)
+    def __init__(self, game, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        if game:
-            self.fields['pitcher'].queryset = pitcher.objects.filter(plays_for=game.home_team)
+
+        set_player = player.objects.get(team=game.away_team)
+
+        self.fields['SO'].initial = set_player.AB
+        self.fields['hits'].initial = set_player.BB
+        self.fields['walks'].initial = set_player.SO
+        self.fields['HR'].initial = set_player.Singles
+        self.fields['IP'].initial = set_player.Doubles
+        self.fields['runs'].initial = set_player.Triples
+        self.fields['ER'].initial = set_player.HR
+        self.fields['games'].initial = set_player.Runs
+        self.fields['GS'].initial = set_player.RBI
+        self.fields['AB'].initial = set_player.SB
